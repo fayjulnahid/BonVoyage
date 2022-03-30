5bd09c4b4324144819ce83e2043aaaa23b177cbe
fayjulnahid <fayjulnahid@gmail.com>
2022-03-31 04:05:18 +0600

    creating and updating user profile

--- BV/main/models.py
+++ BV/main/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+class userProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=300, null=True)
+    user_phone = models.CharField(max_length=20, null=True)
+    user_address = models.CharField(max_length=20, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    bio = models.CharField(max_length=300, null=True)
+    user_image = models.ImageField(blank=True, null=True, upload_to='media', default='user.png')
--- BV/main/urls.py
+++ BV/main/urls.py
@@ -1,8 +1,12 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
 
 app_name = 'main'
 
 urlpatterns = [
-    path('',views.index, name='base_template')
+    path('',views.index, name='base_template'),
+
+    url(r'^user_profile/$', views.UserProfile, name="user_profile"),
+    url(r'^create_profile/$', views.createProfile, name="create_profile"),
 ]
