389e8e025f273eb4f823380ad0982026c52408d4
fayjulnahid <fayjulnahid@gmail.com>
2022-04-24 19:52:15 +0600

    ShoutBox done

--- BV/main/admin.py
+++ BV/main/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import userProfile, RoomModel, HotelReview, HotelReservation, chat, Contact
+from .models import userProfile, RoomModel, HotelReview, HotelReservation, chat, Contact,chatForumMessages
 
 admin.site.register(userProfile)
 admin.site.register(RoomModel)
 admin.site.register(HotelReview)
 admin.site.register(HotelReservation)
 admin.site.register(chat)
+admin.site.register(chatForumMessages)
 admin.site.register(Contact)
